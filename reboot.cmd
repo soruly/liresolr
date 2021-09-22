332d062939bb785998c1bce27557acdc4537978c
soruly <soruly@gmail.com>
2021-09-22 17:54:38 +0800

    Remove old docker build

--- reboot.cmd
+++
@@ -1,7 +0,0 @@
-D:
-cd D:\Java\Libs\solr-6.4.0
-bin\solr stop -all | more
-xcopy C:\Java\Projects\LireSolr\dist\lire.jar D:\Java\Libs\solr-6.4.0\server\solr-webapp\webapp\WEB-INF\lib /Y
-xcopy C:\Java\Projects\LireSolr\dist\liresolr.jar D:\Java\Libs\solr-6.4.0\server\solr-webapp\webapp\WEB-INF\lib /Y
-bin\solr start
-cd C:\Java\Projects\LireSolr
